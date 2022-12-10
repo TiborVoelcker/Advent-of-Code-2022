235d0ecb8e4d52e7dc004117055b267a1f5d4f75
Tibor Völcker <tiborvoelcker@hotmail.de>
2022-12-10 00:41:54 +0000

    Make day 1 code cleaner

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,33 +1,78 @@
 use std::env;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Error, Lines};
+
+struct Elves {
+    buffer: Lines<BufReader<File>>,
+    last_idx: i32,
+}
+
+#[derive(Debug)]
+struct Elf {
+    idx: i32,
+    calories: i32,
+}
+
+impl Iterator for Elves {
+    type Item = Elf;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut calories = 0;
+        while let Some(amount) = self.buffer.next() {
+            let amount = amount.unwrap();
+
+            if amount.is_empty() {
+                self.last_idx += 1;
+                return Some(Elf {
+                    idx: self.last_idx,
+                    calories,
+                });
+            } else {
+                calories += amount.parse::<i32>().unwrap();
+            }
+        }
+        if calories > 0 {
+            return Some(Elf {
+                idx: self.last_idx + 1,
+                calories,
+            });
+        }
+        None
+    }
+}
+
+fn elves(file_path: &String) -> Result<Elves, Error> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    Ok(Elves {
+        buffer: reader.lines(),
+        last_idx: 0,
+    })
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let file_path = &args.get(1).expect("File path missing.");
-    let file = File::open(file_path).expect("Could not open file.");
-    let reader = BufReader::new(file);
+    let file_path = args.get(1).expect("File path missing.");
 
-    let mut highest3 = [0, 0, 0];
-    let mut current = 0;
-    for line in reader.lines() {
-        let line = line.unwrap();
-        match line.parse::<i32>() {
-            Ok(cal) => current += cal,
-            _ => {
-                if line.is_empty() {
-                    if current >= highest3[0] {
-                        let mut temp = [current, highest3[0], highest3[1], highest3[2]];
-                        temp.sort();
-                        highest3 = [temp[1], temp[2], temp[3]];
-                    }
-                    current = 0;
-                }
-            }
-        }
-    }
+    let elves = elves(file_path).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        std::process::exit(1);
+    });
 
-    println!("{:?}", highest3);
-    let sum: i64 = (highest3[0] + highest3[1] + highest3[2]).into();
-    println!("{}", sum);
+    let mut elves: Vec<Elf> = elves.collect();
+    elves.sort_by_key(|e| e.calories);
+
+    println!(
+        "The top elf has {} calories.",
+        elves[elves.len() - 1].calories
+    );
+
+    println!(
+        "The top 3 elves have {:?} calories.",
+        &elves[elves.len() - 3..]
+            .into_iter()
+            .map(|e| e.calories)
+            .sum::<i32>()
+    )
 }
